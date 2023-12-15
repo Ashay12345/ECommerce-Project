a545fbd517f0309bbae4d156fe75273616859e95
Ashay Gupta <ashaygupta4@gmail.com>
2023-12-16 01:29:34 +0530

    Changed the content of the App, SingleProduct and Errorpage files

--- E-Commerce/src/App.jsx
+++ E-Commerce/src/App.jsx
@@ -1,5 +1,10 @@
 import {BrowserRouter, Routes, Route, BrowserRouter} from 'react-router-dom'
 import Home from './Home'
+import About from './About'
+import Products from './Products'
+import Contact from './Contact'
+import ErrorPage from './ErrorPage'
+import SingleProduct from './SingleProduct'
 function App(){
   return (
     <BrowserRouter>
@@ -8,6 +13,8 @@ function App(){
           <Route path='/about'  element={<About/>}/>
             <Route path='/products'  element={<Products/>}/>
               <Route path='/contact'  element={<Contact/>}/>
+                <Route path='/errorpage' element={<ErrorPage/>}/>
+                  <Route path='/singleproduct/:id' element={<SingleProduct/>}/>
       </Routes>
     </BrowserRouter>
   )
