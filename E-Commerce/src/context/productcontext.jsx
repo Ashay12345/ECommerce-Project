bdd504a382d7cbffa30a371aa7fec5fe4e822273
Ashay Gupta <ashaygupta4@gmail.com>
2023-12-16 04:14:28 +0530

    Added complexity to the Context logic

--- E-Commerce/src/context/productcontext.jsx
+++ E-Commerce/src/context/productcontext.jsx
@@ -1,5 +1,11 @@
 import { createContext } from "react";
 
 const AppContext = createContext()
-const AppProvider = ({children}) =>{}
+const AppProvider = ({children}) =>{
+    return(
+        <>
+            <AppContext.Provider value="Ashay Gupta">{children}</AppContext.Provider>
+        </>
+    )
+}
 export {AppProvider}
