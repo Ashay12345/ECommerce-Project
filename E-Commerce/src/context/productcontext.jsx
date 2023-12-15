5c0459e21786260fb2a9c5f3f8e6b5c3b652040f
Ashay Gupta <ashaygupta4@gmail.com>
2023-12-16 05:04:01 +0530

    Fetched product data using the axios

--- E-Commerce/src/context/productcontext.jsx
+++ E-Commerce/src/context/productcontext.jsx
@@ -1,7 +1,16 @@
-import { createContext, useContext} from "react";
-
+import { createContext, useContext, useEffect} from "react";
+import axios from 'axios'
 const AppContext = createContext()
+const API = 'api.pujakaitem.com/api/products'
 const AppProvider = ({children}) =>{
+    const getProducts = async (url) =>{
+        const res = await axios.get(url)
+        const products = await res.data
+    }
+    useEffect((url) => {  
+      getProducts(API)
+    }, [])
+    
     return(
         <>
             <AppContext.Provider value={{myName:"Ashay Gupta"}}>{children}</AppContext.Provider>
