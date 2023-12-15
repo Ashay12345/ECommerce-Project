bc9f82f36550dfc7dd7967be83ae4fdb7bdb2dd7
Ashay Gupta <ashaygupta4@gmail.com>
2023-12-16 01:49:09 +0530

    Added the design code for the Header

--- E-Commerce/src/components/Header.jsx
+++ E-Commerce/src/components/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
 
+  .logo {
+    height: 5rem;
+  }
+`;
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <MainHeader></MainHeader>
+    </div>
   )
 }
 
